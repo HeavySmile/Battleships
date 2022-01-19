d26f12c5d8731fea0f96084b2554e2445635d0c5
HeavySmile <94453087+HeavySmile@users.noreply.github.com>
2022-01-19 13:38:47 +0200

    Minor design changes

--- Battleships/Battleship.cpp
+++ Battleships/Battleship.cpp
@@ -1,5 +1,6 @@
 #include "Battleship.h"
 #include <iostream>
+
 using namespace std;
 
 int TransformBoardChar(char character)
--- Battleships/Battleship.h
+++ Battleships/Battleship.h
@@ -3,22 +3,8 @@
 #include <cstring>
 #include <string>
 #include <fstream>
+#include "Constants.h"
 using namespace std;
-
-const int MAX_SHIPS_AMOUNT = 10;
-const int MAX_SHIP_SIZE = 6;
-const int MAX_6TILE_SHIPS_AMOUNT = 1;
-const int MAX_4TILE_SHIPS_AMOUNT = 2;
-const int MAX_3TILE_SHIPS_AMOUNT = 3;
-const int MAX_2TILE_SHIPS_AMOUNT = 4;
-const int BOARD_WIDTH = 10;
-const int BOARD_HEIGHT = 10;
-
-const int BOARD_FAIL_HIT = 4;
-const int BOARD_HIT = 3;
-const int BOARD_SHIP = 1;
-const int BOARD_COLLISION_AREA = 2;
-const int BOARD_BLANKSPACE = 0;
 
 int TransformBoardChar(char character);
 
